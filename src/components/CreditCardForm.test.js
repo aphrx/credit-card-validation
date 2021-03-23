727ac857318ef4953016828d0d002d46fea48ef6
Aphrx <amalnnath20@yahoo.ca>
2021-03-23 09:49:03 -0400

    reduce repeated test cases

--- src/components/CreditCardForm.test.js
+++ src/components/CreditCardForm.test.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  getByLabelText,
+  getByDisplayValue,
+  getByText,
+  getAllByTestId,
+  getAllByText,
+} from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import CreditCardForm from "./CreditCardForm";
 import validateInfo from "./../validateInfo";
@@ -22,59 +30,50 @@ const null_values = {
   cardPostalCode: null,
 };
 
-const { getByTestId } = render(<CreditCardForm />);
 
 // Check if rendering the form
 describe("test rendering", () => {
-  test("renders correctly", () => {
-    expect(getByTestId("cardName")).toBeTruthy();
-    expect(getByTestId("cardNumber")).toBeTruthy();
-    expect(getByTestId("cardType")).toBeTruthy();
-    expect(getByTestId("cardExpiration")).toBeTruthy();
-    expect(getByTestId("cardSecurityCode")).toBeTruthy();
-    expect(getByTestId("cardPostalCode")).toBeTruthy();
-    expect(getByTestId("validateButton")).toBeTruthy();
-    expect(getByTestId("alertMessage")).toBeTruthy();
+  const elements = [
+    "cardName",
+    "cardNumber",
+    "cardType", 
+    "cardExpiration", 
+    "cardSecurityCode", 
+    "cardPostalCode",
+    "validateButton",
+    "alertMessage"
+  ];
+
+  test.each(elements)("focus on card front", async (element) => {
+    let { getByTestId } = render(<CreditCardForm />);
+
+    expect(getByTestId(element)).toBeTruthy();
   });
+
 });
 
 describe("test form functionality", () => {
-  test("test onChange", async () => {
+  const field = [
+    ["cardName", valid_values.cardName],
+    ["cardNumber", valid_values.cardNumber],
+    ["cardType", valid_values.cardType],
+    ["cardExpiration", valid_values.cardExpiration],
+    ["cardSecurityCode", valid_values.cardSecurityCode],
+    ["cardPostalCode", valid_values.cardPostalCode],
+  ];
+
+  test.each(field)("focus on card front", async (field, values) => {
     let { getByTestId } = render(<CreditCardForm />);
     await act(async () => {
-      await fireEvent.change(getByTestId("cardName"), {
-        target: { value: valid_values.cardName },
-      });
-      await fireEvent.change(getByTestId("cardNumber"), {
-        target: { value: valid_values.cardNumber },
-      });
-      await fireEvent.change(getByTestId("cardType"), {
-        target: { value: valid_values.cardType },
-      });
-      await fireEvent.change(getByTestId("cardExpiration"), {
-        target: { value: valid_values.cardExpiration },
-      });
-      await fireEvent.change(getByTestId("cardSecurityCode"), {
-        target: { value: valid_values.cardSecurityCode },
-      });
-      await fireEvent.change(getByTestId("cardPostalCode"), {
-        target: { value: valid_values.cardPostalCode },
+      await fireEvent.focus(getByTestId(field));
+      await fireEvent.change(getByTestId(field), {
+        target: { value: values },
       });
     });
 
-    expect(getByTestId("cardName").value).toBe(valid_values.cardName);
-    expect(getByTestId("cardNumber").value).toBe(valid_values.cardNumber);
-    expect(getByTestId("cardType").value).toBe(valid_values.cardType);
-    expect(getByTestId("cardExpiration").value).toBe(
-      valid_values.cardExpiration
-    );
-    expect(getByTestId("cardSecurityCode").value).toBe(
-      valid_values.cardSecurityCode
-    );
-    expect(getByTestId("cardPostalCode").value).toBe(
-      valid_values.cardPostalCode
-    );
+    expect(getByTestId(field).value).toBe(values);
   });
+
   test("test onSubmit", () => {
     let { getByTestId } = render(<CreditCardForm />);
     act(() => {
@@ -89,112 +88,112 @@ describe("test form functionality", () => {
 describe("test empty fields", () => {
   test("test null card", () => {
     expect(validateInfo(null_values)).toStrictEqual({
-        message: "Cardholder name is not complete",
-        show: true,
-        variant: "danger",
-        ccvv: false,
-        cexp: false,
-        cname: false,
-        cnumber: false,
-        cpostal: false,
-        ctype: false,
-      });
+      message: "Cardholder name is not complete",
+      show: true,
+      variant: "danger",
+      ccvv: false,
+      cexp: false,
+      cname: false,
+      cnumber: false,
+      cpostal: false,
+      ctype: false,
+    });
   });
 
   test("test empty card name", () => {
     let values = Object.create(valid_values);
     values.cardName = "";
     expect(validateInfo(values)).toStrictEqual({
-        message: "Cardholder name is not complete",
-        show: true,
-        variant: "danger",
-        ccvv: true,
-        cexp: true,
-        cname: false,
-        cnumber: true,
-        cpostal: true,
-        ctype: true,
-      });
+      message: "Cardholder name is not complete",
+      show: true,
+      variant: "danger",
+      ccvv: true,
+      cexp: true,
+      cname: false,
+      cnumber: true,
+      cpostal: true,
+      ctype: true,
+    });
   });
 
   test("test empty card number", () => {
     let values = Object.create(valid_values);
     values.cardNumber = "";
     expect(validateInfo(values)).toStrictEqual({
-        message: "Credit card number is not complete",
-        show: true,
-        variant: "danger",
-        ccvv: true,
-        cexp: true,
-        cname: true,
-        cnumber: false,
-        cpostal: true,
-        ctype: false,
-      });
+      message: "Credit card number is not complete",
+      show: true,
+      variant: "danger",
+      ccvv: true,
+      cexp: true,
+      cname: true,
+      cnumber: false,
+      cpostal: true,
+      ctype: false,
+    });
   });
 
   test("test empty card type", () => {
     let values = Object.create(valid_values);
     values.cardType = "";
     expect(validateInfo(values)).toStrictEqual({
-        message: "Credit card type is not complete",
-        show: true,
-        variant: "danger",
-        ccvv: true,
-        cexp: true,
-        cname: true,
-        cnumber: true,
-        cpostal: true,
-        ctype: false,
-      });
+      message: "Credit card type is not complete",
+      show: true,
+      variant: "danger",
+      ccvv: true,
+      cexp: true,
+      cname: true,
+      cnumber: true,
+      cpostal: true,
+      ctype: false,
+    });
   });
 
   test("test empty card exp", () => {
     let values = Object.create(valid_values);
     values.cardExpiration = "";
     expect(validateInfo(values)).toStrictEqual({
-        message: "Credit card expiration date is not complete",
-        show: true,
-        variant: "danger",
-        ccvv: true,
-        cexp: false,
-        cname: true,
-        cnumber: true,
-        cpostal: true,
-        ctype: true,
-      });
+      message: "Credit card expiration date is not complete",
+      show: true,
+      variant: "danger",
+      ccvv: true,
+      cexp: false,
+      cname: true,
+      cnumber: true,
+      cpostal: true,
+      ctype: true,
+    });
   });
 
   test("test empty card cvv", () => {
     let values = Object.create(valid_values);
     values.cardSecurityCode = "";
     expect(validateInfo(values)).toStrictEqual({
-        message: "Credit card CVV is not complete",
-        show: true,
-        variant: "danger",
-        ccvv: false,
-        cexp: true,
-        cname: true,
-        cnumber: true,
-        cpostal: true,
-        ctype: true,
-      });
+      message: "Credit card CVV is not complete",
+      show: true,
+      variant: "danger",
+      ccvv: false,
+      cexp: true,
+      cname: true,
+      cnumber: true,
+      cpostal: true,
+      ctype: true,
+    });
   });
 
   test("test empty card postal code", () => {
     let values = Object.create(valid_values);
     values.cardPostalCode = "";
     expect(validateInfo(values)).toStrictEqual({
-        message: "Credit card postal code is not complete",
-        show: true,
-        variant: "danger",
-        ccvv: true,
-        cexp: true,
-        cname: true,
-        cnumber: true,
-        cpostal: false,
-        ctype: true,
-      });
+      message: "Credit card postal code is not complete",
+      show: true,
+      variant: "danger",
+      ccvv: true,
+      cexp: true,
+      cname: true,
+      cnumber: true,
+      cpostal: false,
+      ctype: true,
+    });
   });
 });
 
@@ -216,111 +215,111 @@ describe("test valid fields", () => {
 });
 
 describe("test invalid fields", () => {
-    test("test invalid name", () => {
-        let values = Object.create(valid_values);
-        values.cardName = "4111111111111111";
-    
-        const result = {
-          message: "Cardholder name is invalid",
-          show: true,
-          variant: "danger",
-          ccvv: true,
-          cexp: true,
-          cname: false,
-          cnumber: true,
-          cpostal: true,
-          ctype: true,
-        };
-        expect(validateInfo(values)).toStrictEqual(result);
-      });
-    
-      test("test invalid number", () => {
-        let values = Object.create(valid_values);
-        values.cardNumber = "411111111111111111111111";
-        
-        const result = {
-          message: "Credit card number is invalid",
-          show: true,
-          variant: "danger",
-          ccvv: true,
-          cexp: true,
-          cname: true,
-          cnumber: false,
-          cpostal: true,
-          ctype: true,
-        };
-        expect(validateInfo(values)).toStrictEqual(result);
-      });
-    
-      test("test invalid number", () => {
-        let values = Object.create(valid_values);
-        values.cardType = "VC";
-    
-        const result = {
-          message: "Credit card type is invalid",
-          show: true,
-          variant: "danger",
-          ccvv: true,
-          cexp: true,
-          cname: true,
-          cnumber: true,
-          cpostal: true,
-          ctype: false,
-        };
-        expect(validateInfo(values)).toStrictEqual(result);
-      });
-    
-      test("test invalid expiration", () => {
-        let values = Object.create(valid_values);
-        values.cardExpiration = "0505/20222022";
-    
-        const result = {
-          message: "Credit card expiration date is invalid",
-          show: true,
-          variant: "danger",
-          ccvv: true,
-          cexp: false,
-          cname: true,
-          cnumber: true,
-          cpostal: true,
-          ctype: true,
-        };
-        expect(validateInfo(values)).toStrictEqual(result);
-      });
-    
-      test("test invalid cvv", () => {
-        let values = Object.create(valid_values);
-        values.cardSecurityCode = "050520222022";
-    
-        const result = {
-          message: "Credit card CVV is invalid",
-          show: true,
-          variant: "danger",
-          ccvv: false,
-          cexp: true,
-          cname: true,
-          cnumber: true,
-          cpostal: true,
-          ctype: true,
-        };
-        expect(validateInfo(values)).toStrictEqual(result);
-      });
-    
-      test("test invalid postal code", () => {
-        let values = Object.create(valid_values);
-        values.cardPostalCode = "0";
-    
-        const result = {
-          message: "Credit card postal code is invalid",
-          show: true,
-          variant: "danger",
-          ccvv: true,
-          cexp: true,
-          cname: true,
-          cnumber: true,
-          cpostal: false,
-          ctype: true,
-        };
-        expect(validateInfo(values)).toStrictEqual(result);
-      });
-})
+  test("test invalid name", () => {
+    let values = Object.create(valid_values);
+    values.cardName = "4111111111111111";
+
+    const result = {
+      message: "Cardholder name is invalid",
+      show: true,
+      variant: "danger",
+      ccvv: true,
+      cexp: true,
+      cname: false,
+      cnumber: true,
+      cpostal: true,
+      ctype: true,
+    };
+    expect(validateInfo(values)).toStrictEqual(result);
+  });
+
+  test("test invalid number", () => {
+    let values = Object.create(valid_values);
+    values.cardNumber = "411111111111111111111111";
+
+    const result = {
+      message: "Credit card number is invalid",
+      show: true,
+      variant: "danger",
+      ccvv: true,
+      cexp: true,
+      cname: true,
+      cnumber: false,
+      cpostal: true,
+      ctype: true,
+    };
+    expect(validateInfo(values)).toStrictEqual(result);
+  });
+
+  test("test invalid type", () => {
+    let values = Object.create(valid_values);
+    values.cardType = "VC";
+
+    const result = {
+      message: "Credit card type is invalid",
+      show: true,
+      variant: "danger",
+      ccvv: true,
+      cexp: true,
+      cname: true,
+      cnumber: true,
+      cpostal: true,
+      ctype: false,
+    };
+    expect(validateInfo(values)).toStrictEqual(result);
+  });
+
+  test("test invalid expiration", () => {
+    let values = Object.create(valid_values);
+    values.cardExpiration = "0505/20222022";
+
+    const result = {
+      message: "Credit card expiration date is invalid",
+      show: true,
+      variant: "danger",
+      ccvv: true,
+      cexp: false,
+      cname: true,
+      cnumber: true,
+      cpostal: true,
+      ctype: true,
+    };
+    expect(validateInfo(values)).toStrictEqual(result);
+  });
+
+  test("test invalid cvv", () => {
+    let values = Object.create(valid_values);
+    values.cardSecurityCode = "050520222022";
+
+    const result = {
+      message: "Credit card CVV is invalid",
+      show: true,
+      variant: "danger",
+      ccvv: false,
+      cexp: true,
+      cname: true,
+      cnumber: true,
+      cpostal: true,
+      ctype: true,
+    };
+    expect(validateInfo(values)).toStrictEqual(result);
+  });
+
+  test("test invalid postal code", () => {
+    let values = Object.create(valid_values);
+    values.cardPostalCode = "0";
+
+    const result = {
+      message: "Credit card postal code is invalid",
+      show: true,
+      variant: "danger",
+      ccvv: true,
+      cexp: true,
+      cname: true,
+      cnumber: true,
+      cpostal: false,
+      ctype: true,
+    };
+    expect(validateInfo(values)).toStrictEqual(result);
+  });
+});
